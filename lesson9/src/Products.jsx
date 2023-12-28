9b3110215a1b4aba9ba0592c8b8411782d614e29
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2023-12-28 19:59:50 +0500

    added lesson 9 davomi

--- lesson9/src/Products.jsx
+++ lesson9/src/Products.jsx
@@ -45,20 +45,79 @@ const Products = () => {
     },
   ]);
 
+  const handleClear = () => {
+    setProducts([]);
+  };
+
+  function handleRefresh() {
+    setProducts([
+      {
+        id: 1,
+        img: "product1.jpg",
+        title: "product 1",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit id corrupti, mollitia facere quis sint rem quasi. Beatae, facilis!",
+        price: 3000,
+      },
+      {
+        id: 2,
+        img: "product2.jpg",
+        title: "product 2",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit id corrupti, mollitia facere quis sint rem quasi. Beatae, facilis!",
+        price: 7000,
+      },
+      {
+        id: 3,
+        img: "product3.jpg",
+        title: "product 3",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit id corrupti, mollitia facere quis sint rem quasi. Beatae, facilis!",
+        price: 5000,
+      },
+      {
+        id: 4,
+        img: "product4.jpg",
+        title: "product 4",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit id corrupti, mollitia facere quis sint rem quasi. Beatae, facilis!",
+        price: 4000,
+      },
+      {
+        id: 5,
+        img: "product5.jpg",
+        title: "product 5",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus velit id corrupti, mollitia facere quis sint rem quasi. Beatae, facilis!",
+        price: 5000,
+      },
+    ]);
+  }
   return (
-    <div className="flex">
-      {products.map((el) => {
-        return (
-          <ProductItem
-            key={el.id}
-            title={el.title}
-            img={el.img}
-            description={el.description}
-            price={el.price}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="flex">
+        {products.map((el) => {
+          return (
+            <ProductItem
+              key={el.id}
+              title={el.title}
+              img={el.img}
+              description={el.description}
+              price={el.price}
+            />
+          );
+        })}
+        {products.length === 0 && (
+          <p style={{ color: "red", fontSize: 40 }}>Topilmadi!;</p>
+        )}
+      </div>
+      <br />
+      <button onClick={handleClear}>Tozalash</button>
+      <br />
+      <br />
+      <br />
+      <button onClick={handleRefresh}>Qaytarish</button>
+    </>
   );
 };
 
